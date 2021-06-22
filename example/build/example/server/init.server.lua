f0aceffb8f79fe864326562c616f569bda1200df
Jonathan <void.knight09@gmail.com>
2021-06-23 01:09:32 +1200

    Fix example in repo

--- example/build/example/server/init.server.lua
+++
@@ -1,9 +0,0 @@
--- Compiled with roblox-ts v1.0.0
-local TS = require(game:GetService("ReplicatedStorage"):WaitForChild("TS"):WaitForChild("RuntimeLib"))
-local MessageTemplateParser = TS.import(script, game:GetService("ReplicatedStorage"), "TS", "node_modules", "net", "MessageTemplateParser").MessageTemplateParser
-local test = MessageTemplateParser.Parse("Hello, {Name}! How is your {TimeOfDay}?")
-local hour = DateTime.fromUnixTimestamp(os.time()):ToLocalTime().Hour
-print(test:Render({
-	Name = "Vorlias",
-	TimeOfDay = (hour < 6 or hour > 16) and "Night" or "Day",
-}))
